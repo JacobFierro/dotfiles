fee73f2c15892df1e4cfd210df70bef907dd790f
Jacob Fierro <jacob.fierro@gmail.com>
2016-04-13 12:45:12 -0400

    stuff

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,5 @@
 export PATH="$HOME/bin:./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$HOME/Library/Haskell/bin:/usr/local/bin/mysql:/Applications/Postgres.app/Contents/Versions/9.3/bin:$PATH"
-
 export PATH=${PATH}:$HOME/Documents/Work/adt/sdk/platform-tools:$HOME/Documents/Work/adt/sdk/tools
-
 export PATH=${PATH}:/usr/local/lib/node_modules
-export PATH=${PATH}:$HOME/bin
+export PATH=${PATH}:/Applications/Postgres.app/Contents/Versions/latest/bin
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,7 +11,7 @@ alias cleanup="find . -name '*.DS_Store' -type f -ls -delete"
 alias flush="dscacheutil -flushcache"
 
 # Start a webserver in the current dir, stolen from @stevenmbone
-alias servethis="open http://0.0.0.0:8000 && python -m SimpleHTTPServer"
+alias servethis="open http://0.0.0.0:8000 && python -m SimpleHTTPServer 8000"
 
 # Hide/show all desktop icons (useful when presenting)
 alias hidedesktop"defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
@@ -22,6 +22,8 @@ alias stfu="osascript -e 'set volume output muted true'"
 alias pumpitup="osascript -e 'set volume 10'"
 alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
 
+alias editalias="vim ~/.dotfiles/zsh/aliases.zsh"
+
 # Some shortcuts to commonly used places
 alias desk="cd ~/Desktop/"
 alias webdocs="cd /Library/WebServer/Documents/"
@@ -29,13 +31,12 @@ alias work="cd ~/Documents/Work/"
 alias repos="cd ~/Repos/"
 
 alias sails="nocorrect sails"
-alias cinplayer="cd ~/Documents/Work/cinematique/cinematique-player/"
 
 alias vact="source bin/activate"
 alias de="deactivate;"
 
 alias cin="cd ~/Work2/cin/cinematique"
-alias getplayer="cp ~/Work2/cin/mediaelement/local-build/mediaelementplayer.js ~/Work2/cin/cinematique/cin/static_local/app/js/lib/mediaelement"
+alias getplayer="cp ~/Work2/cin/mediaelement/local-build/mediaelementplayer.js ~/Work2/cin/cinematique/cin/static_local/app/js/lib/mediaelement && cp ~/Work2/cin/mediaelement/local-build/mediaelement.js ~/Work2/cin/cinematique/cin/static_local/app/js/lib/mediaelement"
 alias getplayer2="cp ~/Work2/cin/mediaelement/local-build/mediaelementplayer.js ~/Documents/Work/cinematique/cinematique-player/public/player/js/lib/mediaelement"
 alias cinsass="cin; cd cinematique/static_local/app/css; compass watch ."
 
@@ -43,4 +44,12 @@ alias cinrun="CIN_ENV=jacob node ./bin/app.js"
 alias cinqa="cinplayer; grunt update_server_code:app_qa_instance_1.1.1.1:qa:us-east-1"
 alias cindev="cinplayer; grunt update_server_code:app_dev_server_1.1.9.0:dev:us-east-1"
 
-alias gopim="cd ~/Work2/pimmento"
+alias pim="cd ~/Work2/pimmento && ls"
+
+alias pimwebmake="cd ~/Work2/pimmento/pim-web && tmux new -s pim-web"
+alias pimiosmake="cd ~/Work2/pimmento/pim-ios && tmux new -s pim-ios"
+alias pimplatform="cd ~/Work2/pimmento/pim-platform && tmux new -s pim-platform"
+alias pimservice="cd ~/Work2/pimmento/pim-data-access && tmux new -s pim-service"
+alias pimsails="cd ~/Work2/pimmento/pim-sails-test && tmux new -s pim-sails"
+alias pimweb="cd ~/Work2/pimmento/pim-web-app && tmux new -s pim-web-app"
+alias vbmount="sshfs pim:/vagrant /Users/jacobfierro/Work2/pimmento/remotes/pim-web-app"
